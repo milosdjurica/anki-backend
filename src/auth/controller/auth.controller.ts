8cf8327fef5154a23522fa5bce745e9f748aee1d
milosdjurica <milosdjurica13@gmail.com>
2023-03-28 04:28:01 +0200

    auth done

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import {
+  GetCurrentUser,
+  GetCurrentUserId,
+  Public,
+} from '@Src/common/decorators';
+import { RtGuard } from '@Src/common/guards';
+import { PasswordValidationPipe } from '@Src/common/pipes';
 import { LogInDto, RegisterDto } from '../dto';
 import { AuthService } from '../service/auth.service';
 
@@ -6,23 +21,35 @@ import { AuthService } from '../service/auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('local/register')
+  @UsePipes(new PasswordValidationPipe())
+  @HttpCode(HttpStatus.CREATED)
   registerLocal(@Body() registerDto: RegisterDto) {
     return this.authService.registerLocal(registerDto);
   }
 
+  @Public()
   @Post('local/logIn')
+  @HttpCode(HttpStatus.OK)
   logInLocal(@Body() logInDto: LogInDto) {
     return this.authService.logInLocal(logInDto);
   }
 
   @Post('logOut')
-  logOut() {
-    return this.authService.logOut();
+  @HttpCode(HttpStatus.OK)
+  logOut(@GetCurrentUserId() userId: number) {
+    return this.authService.logOut(userId);
   }
 
+  @Public()
+  @UseGuards(RtGuard)
   @Post('refresh')
-  refreshTokens() {
-    return this.authService.refreshTokens();
+  @HttpCode(HttpStatus.OK)
+  refreshTokens(
+    @GetCurrentUserId() userId: number,
+    @GetCurrentUser('refreshToken') refreshToken: string,
+  ) {
+    return this.authService.refreshTokens(userId, refreshToken);
   }
 }
--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from '@Src/prisma/prisma.service';
 import { LogInDto, RegisterDto } from '../dto';
 import * as bcrypt from 'bcrypt';
@@ -9,10 +14,24 @@ import { Tokens } from '../types';
 export class AuthService {
   constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
+  // !REGISTER
   async registerLocal(registerDto: RegisterDto): Promise<Tokens> {
+    const usernameTaken = await this.prisma.user.findUnique({
+      where: {
+        username: registerDto.username,
+      },
+    });
+
+    if (usernameTaken)
+      throw new HttpException(
+        'Username is already taken! Please provide another one.',
+        HttpStatus.BAD_REQUEST,
+      );
+
     const hashPassword = await this.hashData(registerDto.password);
     delete registerDto.confirmPassword;
-    delete registerDto.password
+    delete registerDto.password;
+
     const newUser = await this.prisma.user.create({
       data: {
         hashPassword,
@@ -26,12 +45,78 @@ export class AuthService {
     return tokens;
   }
 
-  logInLocal(logInDto: LogInDto) {}
+  // !LOG IN
+  async logInLocal(logInDto: LogInDto): Promise<Tokens> {
+    const foundUser = await this.prisma.user.findUnique({
+      where: {
+        username: logInDto.username,
+      },
+    });
 
-  logOut() {}
+    if (!foundUser) throw new ForbiddenException('Username not found!');
 
-  refreshTokens() {}
+    const passwordMatches = await bcrypt.compare(
+      logInDto.password,
+      foundUser.hashPassword,
+    );
 
+    if (!passwordMatches)
+      throw new ForbiddenException('Wrong username/password');
+
+    const tokens = await this.getTokens(foundUser.userId, foundUser.username);
+    await this.updateRtHash(foundUser.userId, tokens.refreshToken);
+    return tokens;
+  }
+
+  // !LOG OUT
+  async logOut(userId: number) {
+    // !updateMany because in basic update function cannot access hashedRt
+    const user = await this.prisma.user.updateMany({
+      where: {
+        userId,
+        hashedRt: {
+          not: null,
+        },
+      },
+      data: {
+        hashedRt: null,
+      },
+    });
+
+    if (user.count === 0)
+      return {
+        success: false,
+        msg: 'Something went wrong! User is already logged out!',
+      };
+
+    // !2nd option is to return user object that just logged out
+    return {
+      success: true,
+      msg: `User is successfully logged out!`,
+    };
+  }
+
+  // ! REFRESH TOKENS
+  async refreshTokens(userId: number, refreshToken: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        userId,
+      },
+    });
+    if (!user) throw new ForbiddenException('User not found!');
+
+    if (!user.hashedRt)
+      throw new ForbiddenException('Access denied! You are already logged out');
+
+    const rtMatches = bcrypt.compare(refreshToken, user.hashedRt);
+    if (!rtMatches) throw new ForbiddenException('Token is not valid!');
+
+    const tokens = await this.getTokens(user.userId, user.username);
+    await this.updateRtHash(user.userId, tokens.refreshToken);
+    return tokens;
+  }
+
+  // ! UPDATE REFRESH TOKEN HASH
   async updateRtHash(userId: number, refreshToken: string) {
     const hashedRt = await this.hashData(refreshToken);
     await this.prisma.user.update({
